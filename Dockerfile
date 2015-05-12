0172ac38f73d2f8b236e3d2ed333239408b98736
pnieto <pnieto@teltek.es>
2015-05-12 17:25:15 +0200

    preparado entorno de desarrollo

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+# pnietoiglesias/xensb/Dockerfile
+#
+# VERSION    0.1
+#
+
+FROM ubuntu:14.04
+
+RUN apt-get update && \
+    apt-get -y install python-virtualenv \
+    openssh-client
+
+RUN git clone git@github.com:pnietoiglesias/xensb.git
+
+WORKDIR xensb
---
+++ launch.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+export IMAGE=pnietoiglesias/xensb
+export NAME=xensb
+
+[ -z "$SSH_AUTH_SOCK" ] && eval "$(ssh-agent -s)"
+
+
+build(){
+    echo "Build $IMAGE"
+    docker build -t ${IMAGE} .
+}
+
+debug(){
+   echo "Debug ${IMAGE}"
+   docker run -t -i -v $(readlink -f $SSH_AUTH_SOCK):/ssh-agent -e SSH_AUTH_SOCK=/ssh-agent ${IMAGE} /bin/bash
+}
+
+run(){
+   echo "Run ${IMAGE}"
+   docker kill ${NAME}
+   docker rm ${NAME}
+   docker run -t -i -v $(readlink -f $SSH_AUTH_SOCK):/ssh-agent -e SSH_AUTH_SOCK=/ssh-agent --name ${NAME} ${IMAGE}
+}
+
+usage(){
+    echo "Usage: $0"
+    echo "      -b: build image"
+    echo "      -d: debug image with bash"
+    echo "      -r: run"
+    exit 1;
+}
+
+while getopts ":bhdr" opt; do
+    case $opt in
+        b)
+            build
+            exit 1
+            ;;
+        r)
+            run
+            exit 1
+            ;;
+        d)
+            debug
+            exit 1
+            ;;
+        h)
+            usage
+            exit 1
+            ;;
+        \?)
+            usage
+            exit 1
+            ;;
+    esac
+done
+
+usage
